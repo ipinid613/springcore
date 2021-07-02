4bd8f23eb436b4d7f617b5d633599065b561b581
ipinid613 <ipinid613@kakao.com>
2021-07-02 21:00:22 +0900

    서버 오류 해결(application.properties에 bean-definition-overriding 추가)

--- src/main/java/com/sparta/springcore/ProductRepository.java
+++ src/main/java/com/sparta/springcore/ProductRepository.java
@@ -1,13 +1,14 @@
 package com.sparta.springcore;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 //@Component
 
 // Inversion of Control (IoC) 컨테이너
 // bean 이라고 부름
-//@Component
+@Component
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
